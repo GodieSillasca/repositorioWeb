25fc9d5fa8479ed8492fe9359fd0b4ac7fd8b5c5
GodieSillasca <dcasillasgreco@gmail.com>
2018-10-16 12:36:32 -0500

    Se cambió la versión del piedra, papel o tijera. La versión anterior no era funcional

--- TareaLunes15Octubre/PiedraPapelOTijera/js/app.js
+++ TareaLunes15Octubre/PiedraPapelOTijera/js/app.js
@@ -9,28 +9,29 @@ if (computadoraElige <0.34) {
 }
 
 var comparar = function(eleccion1,eleccion2){
-	if (eleccion2===eleccion1) {
-		return "Empate OwO";
-	}
-	else if (eleccion1==="piedra") {
-		if (eleccion2==="papel") {
-			alert("Te mató el papel");
-		}else{
-			alert("Mataste las tirejas bro 3:");
-		}
-	}else if (eleccion1==="papel") {
-		if (eleccion2==="tijera") {
-			alert("Te mataron las tijeras");
-		}else{
-			alert("Mataste a la piedra bro 3:");
-		}
-	}else if (eleccion1==="tijera") {
+	if (eleccion1===eleccion2) {
+		alert("Empate OwO");
+	}else if (eleccion1!=eleccion2) {
 		if (eleccion2==="piedra") {
-			alert("Te mató la piedra");
-		}else{
-			alert("Mataste al papel bro 3:");
-		
-		}
-	}else{alert("nmms we, no seas tramposo");
+			if (eleccion1==="papel") {
+				alert("Te mató el papel");
+			}else{
+				alert("Mataste las tirejas bro 3:");
+			}
+		}else if (eleccion2==="papel") {
+			if (eleccion1==="tijera") {
+				alert("Te mataron las tijeras");
+			}else{
+				alert("Mataste a la piedra bro 3:");
+			}
+		}else if (eleccion2==="tijera") {
+			if (eleccion1==="piedra") {
+				alert("Te mató la piedra");
+			}else{
+				alert("Mataste al papel bro 3:");
+
+			}
+		}else if (eleccion2 != "tijera" || eleccion2!="papel"||eleccion2!="piedra") {alert("nmms we, no seas tramposo")};
+	}
 }
-console.log(comparar(computadoraElige,usuarioElige));
+	console.log(comparar(computadoraElige,usuarioElige));
